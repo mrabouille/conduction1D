3bf556e2b72d8076e770c908715c3fc902714606
Mickael <mrabouille@users.noreply.github.com>
2025-02-06 13:39:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ for i in range(n_couches):
     st.sidebar.subheader(f"Couche {i + 1}")
     
     # Choisir le matériau pour chaque couche
-    materiau = st.sidebar.selectbox(f"Choisir un matériau pour la couche {i + 1}", list(materiaux.keys()), key=f"{materiaux[i]}")
+    materiau = st.sidebar.selectbox(f"Choisir un matériau pour la couche {i + 1}", list(materiaux.keys()), key=f"{materiaux.keys()[i]}")
     materiaux_couches.append(materiau)
     
     # Choisir l'épaisseur de chaque couche
